0cb3ddfc387b868c0ac376ed5ce26bf87d000701
Kevin COMBRIAT <kevin@sc0ville.com>
2018-08-28 22:53:09 +0900

    feat(decode): Curried by default

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,34 @@ export * from "./safeDecoders";
 export * from "./shared";
 export * from "./unsafeDecoders";
 
-export const decodeString = <T>(decoder: Decoder<T>) => (input: string): T => {
-  let json: any;
+export function decodeString<T>(decoder: Decoder<T>): (input: string) => T;
+export function decodeString<T>(decoder: Decoder<T>, input: string): T;
+export function decodeString<T>(decoder: Decoder<T>, input?: string) {
+  const decode = (input: string) => {
+    let json: unknown;
 
-  try {
-    json = JSON.parse(input);
-  } catch {
-    throw new Error(`Could not parse input: ${input}`);
+    try {
+      json = JSON.parse(input);
+    } catch {
+      throw new Error(`Could not parse input: ${input}`);
+    }
+
+    return decoder(json);
+  };
+
+  if (typeof input !== "undefined") {
+    return decode(input);
   }
 
-  return decoder(json);
-};
+  return decode;
+}
 
-export const decodeValue = <T>(decoder: Decoder<T>) => (input: any): T =>
-  decoder(input);
+export function decodeValue<T>(decoder: Decoder<T>): (input: any) => T;
+export function decodeValue<T>(decoder: Decoder<T>, input: any): T;
+export function decodeValue<T>(decoder: Decoder<T>, input?: any) {
+  if (typeof input !== "undefined") {
+    return decoder(input);
+  }
+
+  return decoder;
+}
